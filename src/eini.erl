ea54085e72a27e2d2c325be2a072a40a6bd564b4
Ryosuke Nakai <nakai@accense.com>
2011-05-18 14:33:20 +0900

    Change filenames

--- src/eini.erl
+++ src/eini.erl
@@ -1,12 +1,12 @@
--module(zucchini).
+-module(eini).
 
 -export([parse_string/1, parse_file/1]).
 
 parse_string(String) when is_binary(String) ->
     parse_string(binary_to_list(String));
 parse_string(String) when is_list(String) ->
-    {ok, Tokens, _} = zucchini_lexer:string(String),
-    zucchini_parser:parse(Tokens).
+    {ok, Tokens, _} = eini:string(String),
+    eini_parser:parse(Tokens).
 
 parse_file(Filename) ->
     case file:read_file(Filename) of
