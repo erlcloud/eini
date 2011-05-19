31b5ad5066a2506dd64bacdc11069c808aad324e
Shunichi Shinohara <shino.shun@gmail.com>
2011-05-19 22:31:44 +0900

    Decompose lex and parse for debug purpose

--- src/eini.erl
+++ src/eini.erl
@@ -18,15 +18,42 @@
 -module(eini).
 
 -export([parse_string/1, parse_file/1]).
+%% for debug use
+-export([lex/1, parse_tokens/1]).
 
 parse_string(String) when is_binary(String) ->
   parse_string(binary_to_list(String));
 parse_string(String) when is_list(String) ->
-  {ok, Tokens, _} = eini_lexer:string(String),
-  eini_parser:parse(Tokens).
+  case lex(String) of
+    {ok, Tokens} ->
+      parse_tokens(Tokens);
+    {error, Reason} ->
+      {error, Reason}
+  end.
 
 parse_file(Filename) ->
   case file:read_file(Filename) of
     {ok, Binary} -> parse_string(Binary);
     Error -> Error
   end.
+
+lex(String) when is_binary(String) ->
+  lex(binary_to_list(String));
+lex(String) when is_list(String) ->
+  %% Add a line break to deal with files which does not end by a blank line.
+  case eini_lexer:string(String ++ "\n") of
+    {ok, Tokens, _} ->
+      {ok, Tokens};
+    ErrorInfo ->
+      {error, ErrorInfo}
+  end.
+  
+parse_tokens(Tokens) ->
+  case eini_parser:parse(Tokens) of
+    {ok, Res} ->
+      {ok, Res};
+    {error, {Line, Mod, Reason}} ->
+      {error, {Line, Mod:format_error(Reason)}}
+  end.
+      
+  
