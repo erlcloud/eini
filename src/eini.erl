3d4c537478f3e9e696ba7e6972a7d73c22cd6615
Shunichi Shinohara <shino.shun@gmail.com>
2011-05-19 22:17:32 +0900

    Cosmetic: indent by 2 spaces

--- src/eini.erl
+++ src/eini.erl
@@ -20,13 +20,13 @@
 -export([parse_string/1, parse_file/1]).
 
 parse_string(String) when is_binary(String) ->
-    parse_string(binary_to_list(String));
+  parse_string(binary_to_list(String));
 parse_string(String) when is_list(String) ->
-    {ok, Tokens, _} = eini_lexer:string(String),
-    eini_parser:parse(Tokens).
+  {ok, Tokens, _} = eini_lexer:string(String),
+  eini_parser:parse(Tokens).
 
 parse_file(Filename) ->
-    case file:read_file(Filename) of
-        {ok, Binary} -> parse_string(Binary);
-        Error -> Error
-    end.
+  case file:read_file(Filename) of
+    {ok, Binary} -> parse_string(Binary);
+    Error -> Error
+  end.
