c0054a2667ad706e3fb1572ba330bc8a420813a2
Shunichi Shinohara <shino.shun@gmail.com>
2011-05-19 21:50:28 +0900

    Add simple test cases

---
+++ test/eini_tests.erl
@@ -0,0 +1,40 @@
+-module(eini_tests).
+
+-author('shino@accense.com').
+
+-include_lib("eunit/include/eunit.hrl").
+
+-import(eini, [parse_string/1]).
+
+setup() ->
+  ok.
+
+teardown(_) ->
+  ok.
+
+empty_test_() ->
+  {setup,
+   fun setup/0,
+   fun teardown/1,
+   [
+    ?_assertEqual([], parse_string("")),
+    ?_assertEqual([], parse_string("\n"))
+   ]}.
+
+empty_one_title_test_() ->
+  {setup,
+   fun setup/0,
+   fun teardown/1,
+   [
+    ?_assertEqual([title, []], parse_string("[title]\n"))
+   ]}.
+
+syntax_error_title_test() ->
+  {setup,
+   fun setup/0,
+   fun teardown/1,
+   [
+    %% セクションタイトルの前に空白
+    ?_assertMatch({error, {_Line, _Module, _Reason}}, parse_string(" [title]"))
+   ]}.
+  
